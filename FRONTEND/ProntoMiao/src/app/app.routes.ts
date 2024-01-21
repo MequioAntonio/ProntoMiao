c52d4540783ed6357932a0fb1641484fd28e64ef
GaetanoEsse <marrapodi990@gmail.com>
2024-01-21 19:48:00 +0100

    WIP Creato visivamente il login

--- FRONTEND/ProntoMiao/src/app/app.routes.ts
+++ FRONTEND/ProntoMiao/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { SignUpComponentComponent } from './components/sign-up-component/sign-up-component.component'; 
+import { LoginComponentComponent } from './components/login-component/login-component.component';
+
 
 export const routes: Routes = [
 
-    {path: 'signup', component : SignUpComponentComponent}
+    {path: 'signup', component : SignUpComponentComponent},
+    {path: 'login', component : LoginComponentComponent}
 
 
 
--- FRONTEND/ProntoMiao/src/app/components/login-component/login-component.component.ts
+++ FRONTEND/ProntoMiao/src/app/components/login-component/login-component.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-login-component',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
   templateUrl: './login-component.component.html',
   styleUrl: './login-component.component.css'
 })
