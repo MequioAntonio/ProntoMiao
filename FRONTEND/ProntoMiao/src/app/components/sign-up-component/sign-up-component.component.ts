a4b5d7c6d26741333c969e3894aef2c39f1171a7
GaetanoEsse <marrapodi990@gmail.com>
2024-01-21 19:42:36 +0100

    Navbar finalmente allineata e logo signup

--- FRONTEND/ProntoMiao/src/app/components/sign-up-component/sign-up-component.component.ts
+++ FRONTEND/ProntoMiao/src/app/components/sign-up-component/sign-up-component.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-sign-up-component',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
   templateUrl: './sign-up-component.component.html',
   styleUrl: './sign-up-component.component.css'
 })
