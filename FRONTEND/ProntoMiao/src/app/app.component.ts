ac8d73ef25624b08e43a4ba6b6f8b4348ac302de
GaetanoEsse <marrapodi990@gmail.com>
2024-01-21 13:03:47 +0100

    Creati component vuoti da popolare

--- FRONTEND/ProntoMiao/src/app/app.component.ts
+++ FRONTEND/ProntoMiao/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ FRONTEND/ProntoMiao/src/app/components/animal-post-component/animal-post-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animal-post-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './animal-post-component.component.html',
+  styleUrl: './animal-post-component.component.css'
+})
+export class AnimalPostComponentComponent {
+
+}
