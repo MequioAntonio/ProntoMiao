60e649c86b57a9fcf3992dafc1959534bcd459c1
DaniAri7 <danielearienzouni@gmail.com>
2024-01-22 18:11:27 +0100

    Update

--- FRONTEND/ProntoMiao/src/app/components/inserimento-segnalazione/inserimento-segnalazione.component.ts
+++ FRONTEND/ProntoMiao/src/app/components/inserimento-segnalazione/inserimento-segnalazione.component.ts
@@ -2,13 +2,19 @@ import {Component} from '@angular/core';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-inserimento-segnalazione',
   standalone: true,
   imports: [
     MatFormFieldModule, 
     MatInputModule, 
-    MatSelectModule
+    MatSelectModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule
   ],
   templateUrl: './inserimento-segnalazione.component.html',
   styleUrl: './inserimento-segnalazione.component.scss'
