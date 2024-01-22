c9bd4f8209a357da6755f7e84053ec4946e1725f
MequioAntonio <mequioantoniowork@gmail.com>
2024-01-22 11:53:42 +0100

    revert del login verso material angular

--- FRONTEND/ProntoMiao/src/app/components/login-component/login-component.component.ts
+++ FRONTEND/ProntoMiao/src/app/components/login-component/login-component.component.ts
@@ -1,37 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import {
+  FormBuilder,
+  FormControl,
   FormsModule,
   ReactiveFormsModule,
-  FormGroup,
-  FormControl,
-  Validators,
 } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import {
+  FloatLabelType,
+  MatFormFieldModule,
+} from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login-component',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule,
+  ],
   templateUrl: './login-component.component.html',
   styleUrl: './login-component.component.scss',
 })
 export class LoginComponentComponent {
-  profileForm = new FormGroup({
-    email: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required]),
-  });
 
-  get f() {
-    return this.profileForm.controls;
-  }
+  constructor(private _formBuilder: FormBuilder) {}
 
-  submit() {
-    console.log(this.profileForm.value);
-  }
 
-  test = true;
-  isValid = this.profileForm.controls['email'].touched;
 }
