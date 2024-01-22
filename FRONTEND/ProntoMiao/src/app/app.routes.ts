2f0e481b1ed8cf6e799f7b276d09f86f0052067f
GaetanoEsse <marrapodi990@gmail.com>
2024-01-22 17:03:07 +0100

    Finito il Frontend del signup e collegato i pulsanti della navbar

--- FRONTEND/ProntoMiao/src/app/app.routes.ts
+++ FRONTEND/ProntoMiao/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { SignUpComponentComponent } from './components/sign-up-component/sign-up-component.component';
 import { LoginComponentComponent } from './components/login-component/login-component.component';
 import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { AnimalPostComponentComponent } from './components/animal-post-component/animal-post-component.component';
 import { InserimentoAnnuncioComponentComponent } from './components/inserimento-annuncio-component/inserimento-annuncio-component.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponentComponent },
   { path: 'signup', component: SignUpComponentComponent },
   { path: 'login', component: LoginComponentComponent },
-  { path: 'inserimento_annuncio', component: InserimentoAnnuncioComponentComponent },
+  { path: 'addadoption', component: InserimentoAnnuncioComponentComponent},
+  { path: 'animalpost', component: AnimalPostComponentComponent}
 ];
--- FRONTEND/ProntoMiao/src/app/components/sign-up-component/sign-up-component.component.ts
+++ FRONTEND/ProntoMiao/src/app/components/sign-up-component/sign-up-component.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-sign-up-component',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatCheckbox, MatButton],
   templateUrl: './sign-up-component.component.html',
   styleUrl: './sign-up-component.component.scss',
 })
