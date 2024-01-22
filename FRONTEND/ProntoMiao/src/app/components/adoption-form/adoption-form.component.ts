7b62bd6752cad73870c650bf40fa2392e3372f80
MequioAntonio <mequioantoniowork@gmail.com>
2024-01-22 20:02:18 +0100

    Rinominato adoption form component seguendo la naming convention di angular

---
+++ FRONTEND/ProntoMiao/src/app/components/adoption-form/adoption-form.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+  FormGroup,
+  AbstractControl,
+} from '@angular/forms';
+import {
+  FloatLabelType,
+  MatFormFieldModule,
+} from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-adoption-form',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule,
+  ],
+  templateUrl: './adoption-form.component.html',
+  styleUrl: './adoption-form.component.scss',
+})
+export class AdoptionFormComponent {
+  pngInputChange(fileInputEvent: any) {
+    console.log(fileInputEvent.target.files[0]);
+  }
+}
