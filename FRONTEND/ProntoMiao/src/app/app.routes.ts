3c90bfe710cba42f10f9c84c91aca4f0033b8c53
MequioAntonio <mequioantoniowork@gmail.com>
2024-01-22 20:17:21 +0100

    nuove rotte post-rinominazione

--- FRONTEND/ProntoMiao/src/app/app.routes.ts
+++ FRONTEND/ProntoMiao/src/app/app.routes.ts
@@ -1,18 +1,18 @@
 import { Routes } from '@angular/router';
-import { SignUpComponentComponent } from './components/sign-up-component/sign-up-component.component';
-import { LoginComponentComponent } from './components/login-component/login-component.component';
-import { HomeComponentComponent } from './components/home-component/home-component.component';
-import { InserimentoSegnalazioneComponent } from './components/inserimento-segnalazione/inserimento-segnalazione.component';
-import { InserimentoAnnuncioComponentComponent } from './components/inserimento-annuncio-component/inserimento-annuncio-component.component';
-import {AnimalCardComponentComponent} from './components/animal-card-component/animal-card-component.component';
-import { AnimalInfoComponentComponent } from './components/animal-info-component/animal-info-component.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { SegnalationFormComponent } from './components/segnalation-form/segnalation-form.component';
+import { AdoptionFormComponent } from './components/adoption-form/adoption-form.component';
+import { HomeComponent } from './components/home/home.component';
+import { AnimalInfoComponent } from './components/animal-info/animal-info.component';
+import { AnimalCardComponent } from './components/animal-card/animal-card.component';
 
 export const routes: Routes = [
-  { path: 'homepage', component: HomeComponentComponent },
-  { path: 'signup', component: SignUpComponentComponent },
-  { path: 'login', component: LoginComponentComponent },
-  { path: 'addadoption', component: InserimentoAnnuncioComponentComponent},
-  { path: 'addreport', component: InserimentoSegnalazioneComponent },
-  { path: 'card', component: AnimalCardComponentComponent },
-  { path: 'info', component: AnimalInfoComponentComponent},
+  { path: 'home', component: HomeComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'adoption-form', component: AdoptionFormComponent },
+  { path: 'segnalation-form', component: SegnalationFormComponent },
+  { path: 'card', component: AnimalCardComponent },
+  { path: 'info', component: AnimalInfoComponent },
 ];
