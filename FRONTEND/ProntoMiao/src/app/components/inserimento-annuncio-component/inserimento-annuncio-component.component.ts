8df8ed0c355258edf5eb69e09a8f0f1687e336b6
MequioAntonio <mequioantoniowork@gmail.com>
2024-01-22 17:25:24 +0100

    updated inserimento annuncio

--- FRONTEND/ProntoMiao/src/app/components/inserimento-annuncio-component/inserimento-annuncio-component.component.ts
+++ FRONTEND/ProntoMiao/src/app/components/inserimento-annuncio-component/inserimento-annuncio-component.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import {
+  FormBuilder,
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+  FormGroup,
+  AbstractControl,
+} from '@angular/forms';
+import {
+  FloatLabelType,
+  MatFormFieldModule,
+} from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-inserimento-annuncio-component',
   standalone: true,
   imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatRadioModule,
     MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule,
   ],
   templateUrl: './inserimento-annuncio-component.component.html',
   styleUrl: './inserimento-annuncio-component.component.scss'
