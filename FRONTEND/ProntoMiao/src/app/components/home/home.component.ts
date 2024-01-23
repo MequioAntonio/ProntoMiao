5a98471904d969d7d1ff2ce9558973f5775d7e03
GaetanoEsse <marrapodi990@gmail.com>
2024-01-23 11:20:30 +0100

    WIP aggiunta la presentazione del sito nella home

--- FRONTEND/ProntoMiao/src/app/components/home/home.component.ts
+++ FRONTEND/ProntoMiao/src/app/components/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { AnimalCardComponent } from '../animal-card/animal-card.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AnimalCardComponent],
+  imports: [AnimalCardComponent, MatTabsModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
