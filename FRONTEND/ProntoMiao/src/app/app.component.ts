c36e9330bb8167e3f0cf225d6f70db18afffc87f
MequioAntonio <mequioantoniowork@gmail.com>
2024-01-23 13:46:09 +0100

    navbar e footer convertiti ad angular material

--- FRONTEND/ProntoMiao/src/app/app.component.ts
+++ FRONTEND/ProntoMiao/src/app/app.component.ts
@@ -1,22 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterOutlet } from '@angular/router';
-import {
-  AbstractControl,
-  FormBuilder,
-  FormGroup,
-  Validators,
-  FormsModule,
-} from '@angular/forms';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HttpClientModule, FormsModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ProntoMiao';
+
 }
