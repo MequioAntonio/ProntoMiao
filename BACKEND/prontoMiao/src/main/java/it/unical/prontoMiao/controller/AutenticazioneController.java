741465e66d7aa91c552d5f7124155e5789212d6a
GaetanoEsse <marrapodi990@gmail.com>
2024-01-24 13:10:19 +0100

    WIP Aggiunta classe di autenticazione da testare

---
+++ BACKEND/prontoMiao/src/main/java/it/unical/prontoMiao/controller/AutenticazioneController.java
@@ -0,0 +1,36 @@
+package it.unical.prontoMiao.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AutenticazioneController implements AuthenticationProvider {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String email = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+
+        if (!password.equals(userDetails.getPassword())) {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+
+        return new UsernamePasswordAuthenticationToken(email, password, userDetails.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
