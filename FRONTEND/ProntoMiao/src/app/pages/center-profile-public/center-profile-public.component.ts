78a06964ce4cd2891c00904a87e5cd44bb8202ee
GaetanoEsse <marrapodi990@gmail.com>
2024-01-26 17:49:21 +0100

    Aggiunti i collegamenti al backend per i centri

--- FRONTEND/ProntoMiao/src/app/pages/center-profile-public/center-profile-public.component.ts
+++ FRONTEND/ProntoMiao/src/app/pages/center-profile-public/center-profile-public.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { AnimalCardComponent } from '../../components/animal-card/animal-card.component';
 import { FeedbackCardComponent } from '../../components/feedback-card/feedback-card.component';
 import { Centro } from '../../model/Centro';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-center-profile-public',
   standalone: true,
@@ -12,5 +15,21 @@ import { Centro } from '../../model/Centro';
 export class CenterProfilePublicComponent {
 
   centro!: Centro;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+
+  ngOnInit(): void {
+
+    this.http.get("http://localhost:8080/centro/"+this.route.snapshot.paramMap.get("idCentro")).subscribe({
+      next:(c:any)=>{
+        this.centro = c;
+      }, 
+      error:(e:any)=>{
+        console.error(e);
+
+      }, 
+    })
+
+  }
 
 }
