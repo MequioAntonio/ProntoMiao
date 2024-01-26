0d42208a65a0b0268c439e6986df195d4ef7dc8c
DaniAri7 <danielearienzouni@gmail.com>
2024-01-26 15:06:58 +0100

    Update

---
+++ FRONTEND/ProntoMiao/src/app/pages/segnalations-page/segnalations-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-segnalations-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './segnalations-page.component.html',
+  styleUrl: './segnalations-page.component.scss'
+})
+export class SegnalationsPageComponent {
+
+}
