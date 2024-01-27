c6de3f4c5c0ce5aaf06834ec677777f54d16fe1f
Filippo Marrapodi <marrapodi990@gmail.com>
2024-01-27 19:15:22 +0100

    Aggiunto il pulsante home alla navbar post login

--- FRONTEND/ProntoMiao/src/app/pages/segnalations-page/segnalations-page.component.ts
+++ FRONTEND/ProntoMiao/src/app/pages/segnalations-page/segnalations-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SegnalationFormComponent } from '../segnalation-form/segnalation-form.component';
 
 @Component({
   selector: 'app-segnalations-page',
   standalone: true,
-  imports: [],
+  imports: [SegnalationFormComponent],
   templateUrl: './segnalations-page.component.html',
   styleUrl: './segnalations-page.component.scss'
 })
