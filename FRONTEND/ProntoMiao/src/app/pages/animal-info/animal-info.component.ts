ce264201d1e87bc9ca2bea6d178aeed2164574b7
DaniAri7 <danielearienzouni@gmail.com>
2024-01-28 14:29:04 +0100

    info component using new service

--- FRONTEND/ProntoMiao/src/app/pages/animal-info/animal-info.component.ts
+++ FRONTEND/ProntoMiao/src/app/pages/animal-info/animal-info.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Annuncio } from '../../model/Annuncio';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { AnnuncioDatabaseService } from '../../services/database-services/annuncio-database.service';
 
 @Component({
   selector: 'app-animal-info',
@@ -12,25 +13,23 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './animal-info.component.html',
   styleUrl: './animal-info.component.scss',
 })
-export class AnimalInfoComponent {
-  annuncio?: Annuncio;
+export class AnimalInfoComponent implements OnInit {
+  annuncio!: Annuncio;
 
-  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+  constructor(private ads: AnnuncioDatabaseService, private router: Router) {}
 
   ngOnInit(): void {
+    const path = this.router.url;
+    const parts = path.split("/");
+    const lastElement = parts[parts.length - 1];
 
-    this.http.get("http://localhost:8080/annuncio/"+this.route.snapshot.paramMap.get("idAnnuncio")).subscribe({
-      next:(a:any)=>{
-        this.annuncio = a;
-      }, 
-      error:(e:any)=>{
-        console.error(e);
 
-      }, 
+    this.ads.getAnnuncioByID(lastElement).subscribe(data=>{
+      this.annuncio = data;
     })
 
   }
 
-   
+
 
 }
