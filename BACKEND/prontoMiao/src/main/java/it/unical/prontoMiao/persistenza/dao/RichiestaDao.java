3f0fa20b2db97ec1196f3091cf84e01bc8f15958
Filippo Marrapodi <marrapodi990@gmail.com>
2024-02-05 18:28:12 +0100

    Creata interfaccia richiesta

--- BACKEND/prontoMiao/src/main/java/it/unical/prontoMiao/persistenza/dao/RichiestaDao.java
+++ BACKEND/prontoMiao/src/main/java/it/unical/prontoMiao/persistenza/dao/RichiestaDao.java
@@ -1,4 +1,16 @@
 package it.unical.prontoMiao.persistenza.dao;
 
+import it.unical.prontoMiao.persistenza.model.Richiesta;
+
+import java.util.List;
+
+
 public interface RichiestaDao {
+    public List<Richiesta> getRichiesta();
+    public Richiesta getRichiestaById(Integer idRichiesta);
+    public List<Richiesta> getRichiestaByCentro(Integer idCentro);
+    public Richiesta insertRichieta (Richiesta richiesta);
+    public void deleteRichiestaById(Integer idRichiesta);
+
+
 }
