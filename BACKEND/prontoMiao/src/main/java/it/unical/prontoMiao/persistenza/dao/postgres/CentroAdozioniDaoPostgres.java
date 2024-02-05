1c165bee72c2fede92d24c058edc579a1df08283
SiriosDev <26876994+SiriosDev@users.noreply.github.com>
2024-02-05 19:43:46 +0100

    aggiunti tutti i get-dao (implementare costruttori mancanti)

--- BACKEND/prontoMiao/src/main/java/it/unical/prontoMiao/persistenza/dao/postgres/CentroAdozioniDaoPostgres.java
+++ BACKEND/prontoMiao/src/main/java/it/unical/prontoMiao/persistenza/dao/postgres/CentroAdozioniDaoPostgres.java
@@ -3,9 +3,17 @@ package it.unical.prontoMiao.persistenza.dao.postgres;
 import it.unical.prontoMiao.model_old.CentroAdozioni;
 import it.unical.prontoMiao.persistenza.dao.CentroAdozioniDao;
 
+import java.sql.Connection;
 import java.util.Optional;
 
 public class CentroAdozioniDaoPostgres implements CentroAdozioniDao {
+
+    Connection conn;
+
+    public CentroAdozioniDaoPostgres(Connection connection) {
+        this.conn=connection;
+    }
+
     @Override
     public CentroAdozioni findCentroById(int id) {
         return null;
