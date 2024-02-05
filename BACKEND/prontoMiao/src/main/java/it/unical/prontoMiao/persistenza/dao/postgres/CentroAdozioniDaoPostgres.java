bc5677c7c962e4c17bd70dffd6842a51137ccda9
Filippo Marrapodi <marrapodi990@gmail.com>
2024-02-05 19:51:05 +0100

    Fatto findCentroById

--- BACKEND/prontoMiao/src/main/java/it/unical/prontoMiao/persistenza/dao/postgres/CentroAdozioniDaoPostgres.java
+++ BACKEND/prontoMiao/src/main/java/it/unical/prontoMiao/persistenza/dao/postgres/CentroAdozioniDaoPostgres.java
@@ -2,13 +2,43 @@ package it.unical.prontoMiao.persistenza.dao.postgres;
 
 import it.unical.prontoMiao.model_old.CentroAdozioni;
 import it.unical.prontoMiao.persistenza.dao.CentroAdozioniDao;
+import it.unical.prontoMiao.persistenza.model.Recensione;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
 
 public class CentroAdozioniDaoPostgres implements CentroAdozioniDao {
+
+    Connection conn;
+
+    public CentroAdozioniDaoPostgres(Connection conn) {
+        this.conn = conn;
+    }
     @Override
     public CentroAdozioni findCentroById(int id) {
-        return null;
+        CentroAdozioni centro = null;
+        String query = "select * from centro_adozioni where id = ?";
+        try {
+            PreparedStatement st = conn.prepareStatement(query);
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                centro = new CentroAdozioni();
+                centro.setId(rs.getInt("id"));
+                centro.setDescrizione(rs.getString("descrizione"));
+                centro.setEventi(rs.getString("eventi"));
+                centro.setIndirizzo(rs.getString("indirizzo"));
+                centro.setNome(rs.getString("nome"));
+                centro.setOrari(rs.getString("orari"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return centro;
     }
 
     @Override
