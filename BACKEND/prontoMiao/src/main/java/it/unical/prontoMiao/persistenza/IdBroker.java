e5699d19ca8043b7a2a5d8f14595ba3b61f94823
SiriosDev <26876994+SiriosDev@users.noreply.github.com>
2024-02-05 16:06:37 +0100

    creati idBroker

--- BACKEND/prontoMiao/src/main/java/it/unical/prontoMiao/persistenza/IdBroker.java
+++ BACKEND/prontoMiao/src/main/java/it/unical/prontoMiao/persistenza/IdBroker.java
@@ -1,4 +1,27 @@
 package it.unical.prontoMiao.persistenza;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class IdBroker {
+
+    private static final String query = "SELECT nextval('db_sequence') AS id";//postgresql
+
+    public static Long getId(Connection connection){
+        Long id = null;
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+
+            ResultSet result = statement.executeQuery();
+            result.next();
+            id = result.getLong("id");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return id;
+    }
+
 }
