b38a17c88b54b7a83186a4f0bd486c615be623fc
SiriosDev <26876994+SiriosDev@users.noreply.github.com>
2024-02-08 19:26:55 +0100

    securety ripristianta

--- BACKEND/prontoMiao/src/main/java/it/unical/prontoMiao/security/SecurityConfiguration.java
+++ BACKEND/prontoMiao/src/main/java/it/unical/prontoMiao/security/SecurityConfiguration.java
@@ -27,7 +27,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request -> request.requestMatchers("/**")
+                .authorizeHttpRequests(request -> request.requestMatchers("/auth/**")
                         .permitAll().requestMatchers("/files/**").permitAll().anyRequest().authenticated())
                 .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider()).addFilterBefore(
