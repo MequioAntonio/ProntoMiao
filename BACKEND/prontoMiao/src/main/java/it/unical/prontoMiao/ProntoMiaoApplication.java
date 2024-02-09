b6ea42318679b73ea4258f4db8a64b7d1bbf08e0
Filippo Marrapodi <marrapodi990@gmail.com>
2024-02-09 11:33:27 +0100

    WIP servlet per visualizzare una table di centri

--- BACKEND/prontoMiao/src/main/java/it/unical/prontoMiao/ProntoMiaoApplication.java
+++ BACKEND/prontoMiao/src/main/java/it/unical/prontoMiao/ProntoMiaoApplication.java
@@ -2,8 +2,10 @@ package it.unical.prontoMiao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class ProntoMiaoApplication {
 
 	public static void main(String[] args) {
