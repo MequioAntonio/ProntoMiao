82a01bf3ff3edd684edc69434bf8a79b912e79e8
Filippo Marrapodi <marrapodi990@gmail.com>
2024-02-09 18:23:45 +0100

    Sistemato anche il Centro

--- FRONTEND/CENTRO/src/app/app.routes.ts
+++ FRONTEND/CENTRO/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { SignUpComponent } from './pages/sign-up/sign-up.component';
+//import { LoginComponent } from './pages/login/login.component';
+//import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { SegnalationFormComponent } from './pages/segnalation-form/segnalation-form.component';
 import { AdoptionFormComponent } from './pages/adoption-form/adoption-form.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -16,8 +16,8 @@ import { SegnalationsPageComponent } from './pages/segnalations-page/segnalation
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'signup', component: SignUpComponent },
-  { path: 'login', component: LoginComponent },
+ // { path: 'signup', component: SignUpComponent },
+  //{ path: 'login', component: LoginComponent },
   { path: 'adoption-form', component: AdoptionFormComponent },
   { path: 'segnalation-form', component: SegnalationFormComponent },
   { path: 'annuncio/:idAnnuncio', component: AnimalInfoComponent },
