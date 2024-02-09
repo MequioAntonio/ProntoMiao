9b64a9edb0718a6b55089e8cbe4aca1401f2c343
SiriosDev <26876994+SiriosDev@users.noreply.github.com>
2024-02-09 11:36:10 +0100

    redirect sul profilo, invece che sulla home

--- FRONTEND/ProntoMiao/src/app/pages/adoption-form/adoption-form.component.ts
+++ FRONTEND/ProntoMiao/src/app/pages/adoption-form/adoption-form.component.ts
@@ -134,7 +134,7 @@ export class AdoptionFormComponent {
     this.annuncioService.insertAnnuncio(annuncio).subscribe((data) => {
       console.log("inserito annuncio!");
       this.snackBar.open("Annuncio creato!","",{duration:3000}).afterDismissed().subscribe(() => {
-        location.href="/";
+        location.href="/private/"+this.authService.getIdUtente();
       });
     })
   }
