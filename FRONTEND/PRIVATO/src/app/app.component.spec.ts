d06d5bcef3c456bf7d28d7b0ef83f443144ce7f5
Filippo Marrapodi <marrapodi990@gmail.com>
2024-02-09 12:58:52 +0100

    Piccola correzione sul path

--- FRONTEND/PRIVATO/src/app/app.component.spec.ts
+++ FRONTEND/PRIVATO/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-privato' title`, () => {
+  it(`should have the 'PRIVATO' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-privato');
+    expect(app.title).toEqual('PRIVATO');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-privato');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, PRIVATO');
   });
 });
