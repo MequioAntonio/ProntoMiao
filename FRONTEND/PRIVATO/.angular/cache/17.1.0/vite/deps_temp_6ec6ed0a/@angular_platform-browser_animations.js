9bc03abca878b748dc31f7b0f0355410ca4d4745
Filippo Marrapodi <marrapodi990@gmail.com>
2024-02-09 18:21:32 +0100

    Ora va anche Privato

---
+++ FRONTEND/PRIVATO/.angular/cache/17.1.0/vite/deps_temp_6ec6ed0a/@angular_platform-browser_animations.js
@@ -0,0 +1,25 @@
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-643BMZFG.js";
+import "./chunk-KZGS4DEZ.js";
+import "./chunk-KAEX55JQ.js";
+import "./chunk-ZKUYKNOU.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-UICRHIOO.js";
+import "./chunk-V2DXGMIT.js";
+import "./chunk-UKEHM6V6.js";
+import "./chunk-ZDOIMVJD.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
